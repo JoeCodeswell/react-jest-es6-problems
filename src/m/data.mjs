49a1b31c0a2d44c4733a9415c9dfa8ddf2207ed4
Joe Dorocak <joecodeswell@gmail.com>
2018-10-20 12:29:31 -0700

    ready to push to github
    Signed-off-by: Joe Dorocak <joecodeswell@gmail.com>

--- src/m/data.mjs
+++ src/m/data.mjs
@@ -1,3 +1,4 @@
+// data.mjs
 export const testlist =  ["one","two","three"];
 export const testobj =  {"k1":"v1", "k2":"v2", "k3":"v3"};
 export const testobjlist =  [
@@ -5,3 +6,4 @@ export const testobjlist =  [
     {"k1":"v1", "k2":"v2", "k3":"v3"},
     {"k4":"v4", "k2":"v2", "k3":"v3"}
 ];
+export default testobjlist;
